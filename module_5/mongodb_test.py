0999087c4fc818c378c2d340894cd6a69b4aa73e
33dragonz <natjeter22@gmail.com>
2023-02-02 20:04:07 -0600

    Here is my finished collections List Example

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,23 @@
+#Natasha Jeter
+#jan 30, 2022
+#Database Security
+#assignment 5.2
+
+
+
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.wk2gxrs.mongodb.net/pytech"
+
+
+client = MongoClient(url)
+
+# connect pytech database
+db = client.pytech  
+
+# show the connected collections 
+print("\n -- Pytech COllection List --")
+print(db.list_collection_names())
+
+# show an exit message
+input("\n\n  End of program, press any key to exit... ")

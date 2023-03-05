f178958d9ad8ac5e21f3bea5d885cb39f5615730
33dragonz <natjeter22@gmail.com>
2023-03-04 22:47:13 -0600

    module_9

--- module_9/pysports_join_queries.py
+++ module_9/pysports_join_queries.py
@@ -1,7 +1,8 @@
+
 """ 
-     pysports_join_queries.py
+  pysports_join_queries.py
    natasha jeter
-   3/4/2023
+    3-4-2023
     Description: Test program for joining the player and team tables
 """
 
@@ -11,7 +12,7 @@ from mysql.connector import errorcode
 
 """ database config object """
 config = {
-    "user": "happy2",
+    "user": "root",
     "password": "Elisea2025!",
     "host": "127.0.0.1",
     "database": "happy",
@@ -25,15 +26,15 @@ try:
 
     cursor = db.cursor()
 
-    
+   
     cursor.execute("SELECT player_id, first_name, last_name, team_name FROM player INNER JOIN team ON player.team_id = team.team_id")
 
- 
+   
     players = cursor.fetchall()
 
-    print("\n  --  show player --")
+    print("\n  -- DISPLAYING PLAYER RECORDS --")
 
-  
+    
     for player in players:
         print("  Player ID: {}\n  First Name: {}\n  Last Name: {}\n  Team Name: {}\n".format(player[0], player[1], player[2], player[3]))
 
